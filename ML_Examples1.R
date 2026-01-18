f1b7e85b599924e5155cbb178d3bc19f7e687465
Mark Montanana <markm8681@hotmail.com>
2026-01-18 20:08:30 +0000

    Add KNN & Naive Bayes Examples

--- ML_Examples1.R
+++ ML_Examples1.R
@@ -1,2 +1,247 @@
 #######ML Examples in R######
-library(gmodels) 
+
+#Load libraries
+library(gmodels)
+library(class)
+library(tm)
+library(SnowballC)
+library(naivebayes)
+library(wordcloud)
+
+#KNN Example: Chapter 3
+## Step 2: Exploring and preparing the data ---- 
+
+# import the CSV file
+wbcd <- read.csv("C:/Data/wisc_bc_data.csv")
+
+# examine the structure of the wbcd data frame
+str(wbcd)
+
+# drop the id feature
+wbcd <- wbcd[-1]
+
+# table of diagnosis
+table(wbcd$diagnosis)
+
+# recode diagnosis as a factor
+wbcd$diagnosis <- factor(wbcd$diagnosis, levels = c("B", "M"),
+                         labels = c("Benign", "Malignant"))
+
+# table or proportions with more informative labels
+round(prop.table(table(wbcd$diagnosis)) * 100, digits = 1)
+
+# summarize three numeric features
+summary(wbcd[c("radius_mean", "area_mean", "smoothness_mean")])
+
+# create normalization function
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+# test normalization function - result should be identical
+normalize(c(1, 2, 3, 4, 5))
+normalize(c(10, 20, 30, 40, 50))
+
+# normalize the wbcd data
+wbcd_n <- as.data.frame(lapply(wbcd[2:31], normalize))
+
+# confirm that normalization worked
+summary(wbcd_n$area_mean)
+
+# create training and test data
+wbcd_train <- wbcd_n[1:469, ]
+wbcd_test <- wbcd_n[470:569, ]
+
+# create labels for training and test data
+
+wbcd_train_labels <- wbcd[1:469, 1]
+wbcd_test_labels <- wbcd[470:569, 1]
+
+## Step 3: Training a model on the data ----
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test,
+                      cl = wbcd_train_labels, k = 21)
+
+## Step 4: Evaluating model performance ----
+
+# Create the cross tabulation of predicted vs. actual
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred,
+           prop.chisq = FALSE)
+
+## Step 5: Improving model performance ----
+
+# use the scale() function to z-score standardize a data frame
+wbcd_z <- as.data.frame(scale(wbcd[-1]))
+
+# confirm that the transformation was applied correctly
+summary(wbcd_z$area_mean)
+
+# create training and test datasets
+wbcd_train <- wbcd_z[1:469, ]
+wbcd_test <- wbcd_z[470:569, ]
+
+# re-classify test cases
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test,
+                      cl = wbcd_train_labels, k = 21)
+
+# Create the cross tabulation of predicted vs. actual
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred,
+           prop.chisq = FALSE)
+
+# try several different values of k
+wbcd_train <- wbcd_n[1:469, ]
+wbcd_test <- wbcd_n[470:569, ]
+
+k_values <- c(1, 5, 11, 15, 21, 27)
+
+# for loop to test all k values in k_values vector
+for (k_val in k_values) {
+  wbcd_test_pred <- knn(train = wbcd_train,
+                        test = wbcd_test,
+                        cl = wbcd_train_labels,
+                        k = k_val)
+  CrossTable(x = wbcd_test_labels,
+             y = wbcd_test_pred,
+             prop.chisq = FALSE)
+} 
+
+##### Chapter 4: Classification using Naive Bayes --------------------
+
+## Example: Filtering spam SMS messages ----
+## Step 2: Exploring and preparing the data ---- 
+
+# read the sms data into the sms data frame
+sms_raw <- read.csv("C:/Data/sms_spam.csv")
+
+# examine the structure of the sms data
+str(sms_raw)
+
+# convert spam/ham to factor.
+sms_raw$type <- factor(sms_raw$type)
+
+# examine the type variable more carefully
+str(sms_raw$type)
+table(sms_raw$type)
+
+# build a corpus using the text mining (tm) package
+sms_corpus <- VCorpus(VectorSource(sms_raw$text))
+
+# examine the sms corpus
+print(sms_corpus)
+inspect(sms_corpus[1:2])
+
+as.character(sms_corpus[[1]])
+lapply(sms_corpus[1:2], as.character)
+
+# clean up the corpus using tm_map()
+sms_corpus_clean <- tm_map(sms_corpus, content_transformer(tolower))
+
+# show the difference between sms_corpus and corpus_clean
+as.character(sms_corpus[[1]])
+as.character(sms_corpus_clean[[1]])
+
+sms_corpus_clean <- tm_map(sms_corpus_clean, removeNumbers) # remove numbers
+sms_corpus_clean <- tm_map(sms_corpus_clean, removeWords, stopwords()) # remove stop words
+sms_corpus_clean <- tm_map(sms_corpus_clean, removePunctuation) # remove punctuation
+
+# tip: create a custom function to replace (rather than remove) punctuation
+removePunctuation("hello...world")
+replacePunctuation <- function(x) { gsub("[[:punct:]]+", " ", x) }
+replacePunctuation("hello...world")
+
+# illustration of word stemming
+wordStem(c("learn", "learned", "learning", "learns"))
+
+sms_corpus_clean <- tm_map(sms_corpus_clean, stemDocument)
+
+sms_corpus_clean <- tm_map(sms_corpus_clean, stripWhitespace) # eliminate unneeded whitespace
+
+# examine the final clean corpus
+lapply(sms_corpus[1:3], as.character)
+lapply(sms_corpus_clean[1:3], as.character)
+
+# create a document-term sparse matrix
+sms_dtm <- DocumentTermMatrix(sms_corpus_clean)
+
+# alternative solution: create a document-term sparse matrix directly from the SMS corpus
+sms_dtm2 <- DocumentTermMatrix(sms_corpus, control = list(
+  tolower = TRUE,
+  removeNumbers = TRUE,
+  stopwords = TRUE,
+  removePunctuation = TRUE,
+  stemming = TRUE
+))
+
+# alternative solution: using custom stop words function ensures identical result
+sms_dtm3 <- DocumentTermMatrix(sms_corpus, control = list(
+  tolower = TRUE,
+  removeNumbers = TRUE,
+  stopwords = function(x) { removeWords(x, stopwords()) },
+  removePunctuation = TRUE,
+  stemming = TRUE
+))
+
+# compare the result
+sms_dtm
+sms_dtm2
+sms_dtm3
+
+# creating training and test datasets
+sms_dtm_train <- sms_dtm[1:4169, ]
+sms_dtm_test  <- sms_dtm[4170:5559, ]
+
+# also save the labels
+sms_train_labels <- sms_raw[1:4169, ]$type
+sms_test_labels  <- sms_raw[4170:5559, ]$type
+
+# check that the proportion of spam is similar
+prop.table(table(sms_train_labels))
+prop.table(table(sms_test_labels))
+
+# word cloud visualization
+wordcloud(sms_corpus_clean, min.freq = 50, random.order = FALSE)
+
+# subset the training data into spam and ham groups
+spam <- subset(sms_raw, type == "spam")
+ham  <- subset(sms_raw, type == "ham")
+
+wordcloud(spam$text, max.words = 40, scale = c(3, 0.5))
+wordcloud(ham$text, max.words = 40, scale = c(3, 0.5))
+
+# indicator features for frequent words
+findFreqTerms(sms_dtm_train, 5)
+
+# save frequently-appearing terms to a character vector
+sms_freq_words <- findFreqTerms(sms_dtm_train, 5)
+str(sms_freq_words)
+
+# create DTMs with only the frequent terms
+sms_dtm_freq_train <- sms_dtm_train[ , sms_freq_words]
+sms_dtm_freq_test <- sms_dtm_test[ , sms_freq_words]
+
+# convert counts to a factor
+convert_counts <- function(x) {
+  x <- ifelse(x > 0, "Yes", "No")
+}
+
+# apply() convert_counts() to columns of train/test data
+sms_train <- apply(sms_dtm_freq_train, MARGIN = 2, convert_counts)
+sms_test  <- apply(sms_dtm_freq_test, MARGIN = 2, convert_counts)
+
+## Step 3: Training a model on the data ----
+sms_classifier <- naive_bayes(sms_train, sms_train_labels)
+
+## Step 4: Evaluating model performance ----
+sms_test_pred <- predict(sms_classifier, sms_test)
+
+
+CrossTable(sms_test_pred, sms_test_labels,
+           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
+## Step 5: Improving model performance ----
+sms_classifier2 <- naive_bayes(sms_train, sms_train_labels, laplace = 1)
+sms_test_pred2 <- predict(sms_classifier2, sms_test)
+CrossTable(sms_test_pred2, sms_test_labels,
+           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
+           dnn = c('predicted', 'actual'))
+
